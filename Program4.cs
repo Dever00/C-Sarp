c6ade3619c5667a330a8b1eb1c6e5bf19a19a9e4
Dever00 <106826856+Dever00@users.noreply.github.com>
2022-09-16 23:20:17 +0500

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,39 @@
+﻿//Верхоланцев Д.А.
+using System;
+
+
+namespace Лаба__4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            try
+            {
+                int x;
+                int y;
+                Console.WriteLine("Введите 1 число: ");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите 2 число: ");
+                y = int.Parse(Console.ReadLine());
+
+
+                int z = Math.BigMul(x, y);
+                Console.WriteLine(x + " * " + y + " = " + z);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Значение выходит за пределы.");
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine("Ошибка преобразования.");
+            }
+            
+        }
+
+
+    }
+}
